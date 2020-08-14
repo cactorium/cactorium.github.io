0252038ea078837f2f825a5cfd6be84a59d818ff
Kelvin Ly <kelvin.ly1618@gmail.com>
2020-08-14 02:21:42 -0400

    Copy link on startup too

--- chess.js
+++ chess.js
@@ -1321,6 +1321,7 @@ function init() {
     document.getElementById("link").value = window.location.protocol + "//" + window.location.host + window.location.pathname + "#" + link_hash
     document.getElementById("link").focus()
     document.getElementById("link").select()
+    document.execCommand("copy")
     send_toast("send the link to your friend so they can play")
   }
 
